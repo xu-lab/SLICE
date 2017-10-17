13f0d785ac3356c7f000e8dffb97b1cffcef0e37
Robrecht Cannoodt <robrecht.cannoodt@ugent.be>
2017-10-17 08:35:42 +0200

    import several functions by default

---
+++ R/package.R
@@ -0,0 +1,9 @@
+#' Determining Cell Differentiation and Lineage based on Single Cell Entropy
+#'
+#' @import ggplot2
+#' @importFrom igraph V E E<- V<-
+#' @importFrom Biobase exprs pData fData pData<- fData<-
+#'
+#' @docType package
+#' @name SLICE-Package
+NULL
