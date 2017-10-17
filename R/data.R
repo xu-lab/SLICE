b91918d17dd4baacf45d3aa26a55cdd140c7ed4f
Robrecht Cannoodt <robrecht.cannoodt@ugent.be>
2017-10-17 08:35:30 +0200

    provide skeleton for documenting data objects

---
+++ R/data.R
@@ -0,0 +1,35 @@
+#' FB
+#'
+#' TODO: add detailed documentation on this code object
+#' @docType data
+"FB"
+
+#' FB
+#'
+#' TODO: add detailed documentation on this code object
+#' @docType data
+"FB"
+
+#' FB.sig
+#'
+#' TODO: add detailed documentation on this code object
+#' @docType data
+"FB.sig"
+
+#' km
+#'
+#' TODO: add detailed documentation on this code object
+#' @docType data
+"km"
+
+#' km_species
+#'
+#' TODO: add detailed documentation on this code object
+#' @docType data
+"km_species"
+
+#' km_version
+#'
+#' TODO: add detailed documentation on this code object
+#' @docType data
+"km_version"
